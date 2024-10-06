ad88f149a17cafb48e32fb986a9fc3d7d144a9fd
PratapSS <pratap.solapur@gmail.com>
2024-10-06 11:29:42 +0530

    Update update_leaderboard.py

--- scripts/update_leaderboard.py
+++ scripts/update_leaderboard.py
@@ -2,6 +2,9 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 # Authenticate with Google Sheets
+with open("credentials.json", 'r') as file_obj:
+    print(file_obj.read())  # Debugging line to show content
+    
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 client = gspread.authorize(creds)
